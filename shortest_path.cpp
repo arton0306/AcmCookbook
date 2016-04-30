bf8ed6416bfa44939bf916a9dd29c737464ad44b
Arton Chang <arton0306@gmail.com>
2016-04-30 04:08:43 -0700

    fix

--- shortest_path.cpp
+++ shortest_path.cpp
@@ -11,35 +11,27 @@ int from[V];
 
 void dijkstra(int s, int t) {
     fill(d, d+V, INT_MAX);
-    d[s] = 0;
-
     fill(used, used+V, false);
-    used[s] = true;
-
+    d[s] = 0;
     from[s] = -1;
-    for (int i = 0; i < V && d[t] < INT_MAX; ++i) {
-        int new_node = -1;
-        int shortest = INT_MAX;
-        for (int j = 0; j < V; ++j) {
-            if (!used[j] && d[j] < shortest) {
-                shortest = d[j];
-                new_node = j;
+    while (true) {
+        int v = -1;
+        for (int u = 0; u < V; ++u) {
+            if (!used[u] && (v == -1 || d[u] < d[v])) {
+                v = u;
             }
         }
-        if (new_node != -1) {
-            for (int j = 0; j < V; ++j) {
-                if (w[new_node][j] < INT_MAX) {
-                    // assume d[new_node] + w[new_node][j] not overflow
-                    if (d[j] > d[new_node] + w[new_node][j]) {
-                        d[j] = d[new_node] + w[new_node][j];
-                        from[j] = new_node;
-                    }
+        if (v == -1) break; // s is not connected to t
+        used[v] = true;
+
+        for (int u = 0; u < V; ++u) {
+            if (w[v][u] < INT_MAX) {
+                // assume d[v] + w[v][u] not overflow
+                if (d[u] > d[v] + w[v][u]) {
+                    d[u] = d[v] + w[v][u];
+                    from[u] = v;
                 }
             }
-            used[new_node] = true;
-        } else {
-            // s is not connected to t
-            break;
         }
     }
 }
